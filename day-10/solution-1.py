638159eda6cb417b7a4770d8b2016d0aee6023a4
alexkallai <scythe1092@gmail.com>
2023-12-12 00:03:23 +0100

    Day 10 WIP still

--- day-10/solution-1.py
+++ day-10/solution-1.py
@@ -17,14 +17,31 @@ width = len(file_lines_list[0])
 height = len(file_lines_list)
 mask_matrix = [[False for _ in range(height)] for _ in range(width)]
 
+class FieldElement:
+    
+    def __init__(self, char, masked, distance) -> None:
+        self.char = char
+        self.masked = masked
+        self.distance = distance
+    
+    def __repr__(self) -> str:
+        return self.char
+
+    def __str__(self) -> str:
+        return f"{self.char}, {self.masked}, {self.distance}"
+
+field: list[list[FieldElement]] = [[None for _ in range(height)] for _ in range(width)]
+for index, line in enumerate(file_lines_list):
+    for i2, char in enumerate(line):
+        field[index][i2] = FieldElement(char, False, None)
 
 # order: x, y > x is horizontal
 val_nb_dict = {
     "|": ((0,-1), (0,1)),
     "-": ((-1,0), (1,0)),
-    "L": ((1,1), (1,0)),
-    "J": ((1,1), (-1,0)),
-    "F": ((0,-1), (1,0)),
+    "L": ((0,-1), (1,0)),
+    "J": ((0,-1), (-1,0)),
+    "F": ((0,1), (1,0)),
 }
 # . is ground
 # S is starting point
@@ -33,7 +50,27 @@ for index, line in enumerate(file_lines_list):
     if "S" in line:
         starting_point = (line.index("S"), index)
 
+def print_matrix(cur_coords:tuple, checked_coords: tuple):
+    #copied_matrix = copy.deepcopy(file_lines_list)
+    copied_matrix = copy.deepcopy(field)
+    for i1, line in enumerate(copied_matrix):
+        for i2, _ in enumerate(line):
+            if i1 == checked_coords[1] and i2 == checked_coords[0]:
+                copied_matrix[i1][i2].char =  "▮"
+            if i1 == cur_coords[1] and i2 == cur_coords[0]:
+                copied_matrix[i1][i2].char =  "?"
+        print(line)
+    print("")
+
 #mask_matrix[starting_point[1]][starting_point[0]] = True
+def is_neighbour_valid(x_coord, y_coord):
+    if 0 <= x_coord < width and 0 <= y_coord < height and field[y_coord][x_coord].masked == False:
+        checked_nb_char = field[y_coord][x_coord].char
+        print(f"Checked char: {checked_nb_char}")
+        if checked_nb_char in val_nb_dict.keys():
+            return True
+    return False
+
 
 def get_first_valid_neighbour(coordinate: tuple):
     for x in range(-1, 2, 1):
@@ -42,14 +79,19 @@ def get_first_valid_neighbour(coordinate: tuple):
                 x_coord = coordinate[0] + x
                 y_coord = coordinate[1] + y
                 #print(x_coord, y_coord)
-                if 0 <= x_coord < width and 0 <= y_coord < height and mask_matrix[y_coord][x_coord] == False:
-                    checked_nb = file_lines_list[y_coord][x_coord]
-                    print(checked_nb)
-                    if checked_nb in val_nb_dict.keys():
-                        for neighbour in val_nb_dict[checked_nb]:
-                            if (x_coord+neighbour[0], y_coord+neighbour[1]) == coordinate:
-                                mask_matrix[y_coord][x_coord] = True
+                if is_neighbour_valid(x_coord, y_coord):
+                    checked_nb_char = field[y_coord][x_coord].char
+                    for neighbour in val_nb_dict[checked_nb_char]:
+                        neighbour_tuple = (x_coord+neighbour[0], y_coord+neighbour[1])
+                        if 0 <= neighbour_tuple[1] < height and 0 <= neighbour_tuple[0] < width:
+                            print_matrix(cur_coords=coordinate ,checked_coords=neighbour_tuple)
+                            if neighbour_tuple  == coordinate:
+                                field[y_coord][x_coord].masked = True
+                                field[y_coord][x_coord].distance = runner
                                 return (x_coord, y_coord)
+                    if checked_nb_char == "S" and runner > 2:
+                        return -1
+    print("ERROR")
 
 #get_first_valid_neighbour(starting_point)
 curr_coord = None
@@ -58,9 +100,14 @@ while curr_coord != starting_point:
     # Init step
     if curr_coord == None:
         curr_coord = starting_point
+        field[curr_coord[1]][curr_coord[0]].masked = True
+        runner += 1
 
     curr_coord = get_first_valid_neighbour(curr_coord)
+    if curr_coord == -1:
+        break
     runner += 1
-    print(runner)
+    #print(runner)
 
+pass
 
